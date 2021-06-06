91ffa1ab4696808a2c29c657b58cc498fb1ecf4c
elfer17 <elderandrade064@gmail.com>
2021-06-05 21:00:52 -0600

    Fin del Curso

---
+++ src/containers/App.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header.jsx';
+import Search from '../components/Search.jsx';
+import Categories from '../components/Categories.jsx';
+import Carousel from '../components/Carousel.jsx';
+import CarouselItem from '../components/CarouselItem.jsx';
+import Footer from '../components/Footer.jsx';
+import useInitialState from '../hooks/useInitialSatate.js';
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState/';
+const App = () => {
+  const initialState = useInitialState(API);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {initialState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Tendencias'>
+        <Carousel>
+          {initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+
+      <Categories title='Originales de Platzi Video'>
+        <Carousel>
+          {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
